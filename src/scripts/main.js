f5a9d0faaaff0c46d6db5bf034d668e5624d13e4
pedro <pdohfg@gmail.com>
2025-04-08 18:37:13 -0300

    enviando o projeto pronto ou quase

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const questions = document.querySelectorAll('[data-faq-question]');
+    const btnTopo = document.getElementById("btnTopo");
 
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
+
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+        btnTopo.classList.add("visible");
+        } else {
+        btnTopo.classList.remove("visible");
+        }
+    });
+
+
 })
 
 function abreOuFechaResposta(elemento) {
